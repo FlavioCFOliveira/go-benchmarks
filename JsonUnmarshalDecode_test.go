329e2ac33f6426a4cfc19b1e9cc8499e69cdc433
Flávio CF Oliveira <flavio-c-oliveira@telecom.pt>
2025-02-24 09:39:36 +0000

    refactor: consolidate JSON benchmark tests and remove obsolete files

--- JsonUnmarshalDecode_test.go
+++ JsonUnmarshalDecode_test.go
@@ -2,6 +2,7 @@ package bench_test
 
 import (
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -42,4 +43,43 @@ func TestDecodeJSONFile(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to decode JSON data: %v", err)
 	}
+}
+
+func BenchmarkJsonDecode(b *testing.B) {
+
+	// Open the JSON file
+	file, err := os.Open("testdata/lx-001.json")
+	if err != nil {
+		b.Fatalf("Failed to open JSON file: %v", err)
+	}
+
+	// Create a new JSON decoder
+	decoder := json.NewDecoder(file)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		file.Seek(0, io.SeekStart)
+
+		var obj []model.Data
+		decoder.Decode(&obj)
+	}
+	b.StopTimer()
+
+	file.Close()
+}
+
+func BenchmarkJsonUnmarshal(b *testing.B) {
+
+	data, err := os.ReadFile("testdata/lx-001.json")
+	if err != nil {
+		b.Fatalf("Failed to read JSON file: %v", err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var obj []model.Data
+		json.Unmarshal(data, &obj)
+	}
+	b.StopTimer()
+
 }
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # go-benchmarks
 
 ```shell
-go test -benchmem -run=^$ -bench ^Benchmark github.com/FlavioCFOliveira/go-benchmarks
+go test -benchmem -run=^$ -bench ^Benchmark -benchtime 30s github.com/FlavioCFOliveira/go-benchmarks
 goos: windows
 goarch: amd64
 pkg: github.com/FlavioCFOliveira/go-benchmarks
 cpu: Intel(R) Core(TM) i7-10510U CPU @ 1.80GHz
-Benchmark_JSONFile_JustDecode-8         669732553                1.763 ns/op           0 B/op          0 allocs/op
-Benchmark_JSONFile_JustUnmarshal-8           1114               987213 ns/op       27669 B/op       2244 allocs/op
+BenchmarkJsonDecode-8              36709           1002144 ns/op          158176 B/op       2248 allocs/op
+BenchmarkJsonUnmarshal-8           36458           1065536 ns/op          158473 B/op       2255 allocs/op
 PASS
-ok      github.com/FlavioCFOliveira/go-benchmarks       2.770s
+ok      github.com/FlavioCFOliveira/go-benchmarks       95.720s
 ```
